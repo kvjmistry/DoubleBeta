d2217fe04d0653db0ce8a5bea893f28290306641
Krishan Mistry <kvjmistry@gmail.com>
2024-01-22 20:44:06 -0600

    Add lowTh file creators

---
+++ Bremsstrahlung/collect_lowTh.py
@@ -0,0 +1,260 @@
+import sys,os,os.path
+# sys.path.append("../../../") # if you move files around, you need to adjust this!
+# sys.path.append(os.path.expanduser('~/code/eol_hsrl_python'))
+# os.environ['ICTDIR']='/home/e78368jw/Documents/NEXT_CODE/IC'
+
+# os.environ["ICTDIR"] = "~/Packages/IC"
+# os.environ["ICDIR"] = "$ICTDIR/invisible_cities"
+
+
+#%load_ext autoreload
+#%autoreload 2
+#%matplotlib notebook
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['mathtext.fontset'] = 'stix'
+rcParams['font.family'] = 'STIXGeneral'
+rcParams['figure.figsize'] = [10, 8]
+rcParams['font.size'] = 22
+
+import pandas as pd
+import numpy  as np
+import tables as tb
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.colors as clrs
+
+import invisible_cities.core.core_functions                   as     coref
+import invisible_cities.io.dst_io                           as     dstio
+
+from invisible_cities.cities                 import beersheba as beerfun
+
+from invisible_cities.evm.event_model                          import HitCollection
+
+from invisible_cities.database.load_db       import DataSiPM
+
+from invisible_cities.evm.event_model        import Cluster, Hit
+from invisible_cities.types.ic_types         import xy
+from invisible_cities.reco.paolina_functions import voxelize_hits
+
+from invisible_cities.evm.event_model        import HitEnergy
+from invisible_cities.cities.beersheba          import DeconvolutionMode
+from invisible_cities.cities.beersheba          import CutType
+
+from invisible_cities.reco import hits_functions as hif
+
+from invisible_cities.reco.deconv_functions import deconvolve
+from invisible_cities.reco.deconv_functions import deconvolution_input
+from invisible_cities.reco.deconv_functions import InterpolationMethod
+
+import invisible_cities.io.mcinfo_io as mcio
+
+from collections import defaultdict
+
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
+
+
+def threshold_hits(threshold_charge, same_peak, hitc):
+    """
+    Applies a threshold to hits and redistributes the charge/energy.
+
+    Parameters
+    ----------
+    threshold_charge : float
+        minimum pes of a hit
+    same_peak        : bool
+        whether to reassign NN hits' energy only to the hits from the same peak
+
+    Returns
+    ----------
+    A function that takes HitCollection as input and returns another object with
+    only non NN hits of charge above threshold_charge.
+    The energy of NN hits is redistributed among neighbors.
+    """
+
+    t = hitc.time
+    thr_hits = hif.threshold_hits(hitc.hits, threshold_charge     )
+    mrg_hits = hif.merge_NN_hits ( thr_hits, same_peak = same_peak)
+
+    cor_hits = []
+    for hit in mrg_hits:
+        cluster = Cluster(hit.Q, xy(hit.X, hit.Y), hit.var, hit.nsipm)
+        xypos   = xy(hit.Xpeak, hit.Ypeak)
+        hit     = Hit(hit.npeak, cluster, hit.Z, hit.E, xypos, hit.Ec)
+        cor_hits.append(hit)
+
+    new_hitc      = HitCollection(hitc.event, t)
+    new_hitc.hits = cor_hits
+    return new_hitc
+
+def hits_from_df (dst, skip_NN = False):
+    """
+    Function that transforms pandas DataFrame dst to HitCollection
+    ------
+    Parameters
+    ------
+    dst : pd.DataFrame
+        DataFrame with obligatory columns :
+                event, npeak, X, Y, Z,  Q, E
+        If time, nsipm, Xrms, Yrms, Qc, Ec, track_id are not
+        inside dst the default value is set to -1
+        If Xpeak, Ypeak not in dst the default value is -1000
+    ------
+    Returns
+    ------
+    Dictionary {event_number : HitCollection}
+    from here
+    https://github.com/next-exp/IC/blob/v2-development/invisible_cities/io/hits_io.py#L16
+    """
+    all_events = {}
+    times = getattr(dst, 'time', [-1]*len(dst))
+    for (event, time) , df in dst.groupby(['event', times]):
+        #pandas is not consistent with numpy dtypes so we have to change it by hand
+        event = np.int32(event)
+        hits  = []
+        for i, row in df.iterrows():
+            Q = getattr(row,'Q', row.E)
+            if skip_NN and Q == NN:
+                continue
+            if hasattr(row, 'Xrms'):
+                Xrms  = row.Xrms
+                Xrms2 = Xrms**2
+            else:
+                Xrms = Xrms2 = -1
+            if hasattr(row, 'Yrms'):
+                Yrms  = row.Yrms
+                Yrms2 = Yrms**2
+            else:
+                Yrms = Yrms2 = -1
+            nsipm   = getattr(row, 'nsipm'   , -1   )     # for backwards compatibility
+            Qc      = getattr(row, 'Qc'      , -1   )     # for backwards compatibility
+            Xpeak   = getattr(row, 'Xpeak'   , -1000)     # for backwards compatibility
+            Ypeak   = getattr(row, 'Ypeak'   , -1000)     # for backwards compatibility
+            Ec      = getattr(row, 'Ec'      , -1   )     # for backwards compatibility
+            trackID = getattr(row, 'track_id', -1   )     # for backwards compatibility
+            Ep      = getattr(row, "Ep"      , -1   )     # for backwards compatibility
+
+            hit = Hit(row.npeak            ,
+                      Cluster(Q               ,
+                              xy(row.X, row.Y),
+                              xy(Xrms2, Yrms2),
+                              nsipm = nsipm   ,
+                              z     = row.Z   ,
+                              E     = row.E   ,
+                              Qc    = Qc      ),
+                      row.Z                ,
+                      row.E                ,
+                      xy(Xpeak, Ypeak)     ,
+                      s2_energy_c = Ec     ,
+                      track_id    = trackID,
+                      Ep          = Ep     )
+
+            hits.append(hit)
+
+        if len(hits):
+            all_events[event] = HitCollection(event, time, hits=hits)
+
+    return all_events
+
+def hitc_to_df_(hitc):
+    columns = defaultdict(list)
+    for hit in hitc.hits:
+        columns["event"   ].append(hitc.event)
+        columns["time"    ].append(hitc.time)
+        columns["npeak"   ].append(hit .npeak)
+        columns["Xpeak"   ].append(hit .Xpeak)
+        columns["Ypeak"   ].append(hit .Ypeak)
+        columns["nsipm"   ].append(hit .nsipm)
+        columns["X"       ].append(hit .X)
+        columns["Y"       ].append(hit .Y)
+        columns["Xrms"    ].append(hit .Xrms)
+        columns["Yrms"    ].append(hit .Yrms)
+        columns["Z"       ].append(hit .Z)
+        columns["Q"       ].append(hit .Q)
+        columns["E"       ].append(hit .E)
+        columns["Qc"      ].append(hit .Qc)
+        columns["Ec"      ].append(hit .Ec)
+        columns["track_id"].append(hit .track_id)
+        columns["Ep"      ].append(hit .Ep)
+    return pd.DataFrame(columns)
+
+def soph_to_lowTh(df, threshold = 5):
+    '''
+    Converts sophronia 'RECO/Events' to lowTh events via a rather convoluted process
+
+    ------
+    Parameters
+    ------
+    df : pd.DataFrame
+        DataFrame with obligatory columns :
+                event, npeak, X, Y, Z,  Q, E
+    threshold: int
+        value at which the threshold is set.
+    ------
+    Returns
+    ------
+    Dictionary {event_number : HitCollection}
+    from here
+    '''
+
+    # safety check, to ensure you don't accidentally make a repeating dataframe
+    
+
+
+    # new parameters for threshold, this is silly but I'm copying previous convention
+    pes = 1
+    threshold = threshold * pes
+    same_peak = True
+
+    # convert sophronia RECO/Events to hit collection
+    soph_hitc = hits_from_df(df)
+
+    # collect the keys as the event numbers
+    soph_hitc_list = list(soph_hitc.keys())
+
+    print("Processing data...")
+    # loop over all of these events
+    j = 0
+    for i in soph_hitc_list:
+        j += 1
+
+        if (len(soph_hitc_list)%j == 50): 
+            print("{}/{}".format(j, len(soph_hitc_list)))
+        # choose i'th event
+        soph_hit_event = soph_hitc.get(i)
+
+        # Apply threshold calculation
+        soph_hitc_lowTh = threshold_hits(threshold, same_peak, soph_hit_event)
+
+        # convert back to pandas dataframe using hitc_to_df
+        soph_hdst_lowTh = hitc_to_df_(soph_hitc_lowTh)
+
+        # check if pandas dataframe with all the events exists yet
+        if 'full_soph_df' in locals() and isinstance(full_soph_df, pd.DataFrame):
+            full_soph_df = pd.concat([full_soph_df, soph_hdst_lowTh])
+        else:
+            full_soph_df = soph_hdst_lowTh.copy(deep = True)
+    
+    return full_soph_df
+
+
+# new parameters for threshold
+pes = 1
+threshold = 5 * pes
+same_peak = True
+
+# loading sophronia file
+soph_file = sys.argv[1]
+soph_hdst = dstio.load_dst(soph_file, 'RECO', 'Events')#CHITS/lowTh
+# print(soph_hdst)        
+
+soph_lowTh = soph_to_lowTh(soph_hdst, threshold = 5)
+
+print(soph_lowTh)
+
+with pd.HDFStore(f"LowTh.h5", mode='w', complevel=5, complib='zlib') as store:
+    # Write each DataFrame to the file with a unique key
+    store.put('lowTh', soph_lowTh, format='table')
