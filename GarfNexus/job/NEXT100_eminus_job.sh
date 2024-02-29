d7ff84e54f24cb57d68f7d3830e469412391ef15
Krishan Mistry <kvjmistry@gmail.com>
2024-02-28 23:13:50 -0600

    Add config files

---
+++ GarfNexus/job/NEXT100_eminus_job.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Starting Job" 
+
+JOBID=$1
+echo "The JOBID number is: ${JOBID}" 
+
+JOBNAME=$2
+echo "The JOBNAME number is: ${JOBNAME}" 
+
+echo "JOBID $1 running on `whoami`@`hostname`"
+start=`date +%s`
+
+SCRIPT=$3
+echo "Script name is: ${SCRIPT}"
+start=`date +%s`
+
+# Setup nexus
+echo "Setting Up NEXUS" 
+source /software/nexus/setup_nexus.sh
+
+# Set the configurable variables
+N_EVENTS=5
+CONFIG=${JOBNAME}.config.mac
+INIT=${JOBNAME}.init.mac
+
+echo "N_EVENTS: ${N_EVENTS}"
+
+SEED=$((${N_EVENTS}*${JOBID} + ${N_EVENTS}))
+echo "The seed number is: ${SEED}" 
+
+# Change the config in the files
+sed -i "s#.*random_seed.*#/nexus/random_seed ${SEED}#" ${CONFIG}
+sed -i "s#.*start_id.*#/nexus/persistency/start_id ${SEED}#" ${CONFIG}
+
+# Print out the config and init files
+cat ${INIT}
+cat ${CONFIG}
+
+# NEXUS
+echo "Running NEXUS" 
+nexus -n $N_EVENTS ${INIT}
+
+ls -ltrh
+
+echo "FINISHED....EXITING" 
+
+end=`date +%s`
+let deltatime=end-start
+let hours=deltatime/3600
+let minutes=(deltatime/60)%60
+let seconds=deltatime%60
+printf "Time spent: %d:%02d:%02d\n" $hours $minutes $seconds 
