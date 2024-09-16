a48acca79d86e60bcb7d50dbafa1d627b982ad31
Krishan Mistry <kvjmistry@gmail.com>
2024-09-16 11:50:59 -0500

    Add batch running of irene

---
+++ Alphas/job/RunIrene.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -J IRENE # A single job name for the array
+#SBATCH --nodes=1
+#SBATCH --mem 4000 # Memory request (6Gb)
+#SBATCH -t 0-1:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o IRENE_%A_%a.out # Standard output
+#SBATCH -e IRENE_%A_%a.err # Standard error
+
+start=`date +%s`
+
+# Setup nexus and run
+echo "Setting up IC"
+source /home/argon/Projects/Krishan/IC/setup_IC.sh
+
+mkdir -p /media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/irene/13850/job${SLURM_ARRAY_TASK_ID}
+cd       /media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/irene/13850/job${SLURM_ARRAY_TASK_ID}
+
+
+input_file=$(sed -n "${SLURM_ARRAY_TASK_ID}p" home/argon/Projects/Krishan/DoubleBeta/Alphas/eventlists/run_13850_files.txt)
+
+city irene /home/argon/Projects/Krishan/DoubleBeta/Alphas/config/irene.conf -i /media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/raw/13850/$input_file -o run_13850_irene_${SLURM_ARRAY_TASK_ID}.h5
+
+
+ls -ltrh 
+
+echo "FINISHED....EXITING"
+
+end=`date +%s`
+let deltatime=end-start
+let hours=deltatime/3600
+let minutes=(deltatime/60)%60
+let seconds=deltatime%60
+printf "Time spent: %d:%02d:%02d\n" $hours $minutes $seconds 
