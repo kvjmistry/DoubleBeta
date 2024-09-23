8b6c374db9d21212c31e0d41ec29382d269199c3
Krishan Mistry <kvjmistry@gmail.com>
2024-09-23 09:56:41 -0500

    Add Kr config files

---
+++ Kr/config/run.sh
@@ -0,0 +1,3 @@
+city irene irene.conf -i ../../data/DEMO/raw/run_12081_0000_trigger1_waveforms.h5 -o run_12081_0000_trigger1_irene.h5
+city dorothea dorothea.conf -i run_12081_0000_trigger1_irene.h5 -o run_12081_0000_trigger1_dorothea.h5
+
