094fbd0ad1da93686b95ebd46ef4c4cacd00787e
Krishan Mistry <kvjmistry@gmail.com>
2024-11-27 17:50:40 -0600

    Split notebooks

--- Alphas/job/merge_files.py
+++ Alphas/job/merge_files.py
@@ -12,20 +12,20 @@ outfile=f"Run_{RUN_NUMBER}_Filtered.h5"
 file_paths = glob.glob(os.path.join(directory_path, '*.h5'))
 print(len(file_paths))
 
-data = []
+# data = []
 data_properties = []
 
 for file_path in file_paths:
-    data.append(pd.read_hdf(file_path, key = 'data'))
+    # data.append(pd.read_hdf(file_path, key = 'data'))
     data_properties.append(pd.read_hdf(file_path, key = 'data_properties'))
 
-data = pd.concat(data)
+# data = pd.concat(data)
 data_properties  = pd.concat(data_properties)
 
-print(data)
+# print(data)
 print(data_properties)
 
 # Open the HDF5 file in write mode
 with pd.HDFStore(outfile, mode='w', complevel=5, complib='zlib') as store:
-    store.put('data',data, format='table')
+    # store.put('data',data, format='table')
     store.put('data_properties',data_properties, format='table')
