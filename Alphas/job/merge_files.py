0720a006c3a53eece1bf174595dac2a3ddc574a0
Krishan Mistry <kvjmistry@gmail.com>
2024-11-29 10:18:37 -0600

    Add merged file update

--- Alphas/job/merge_files.py
+++ Alphas/job/merge_files.py
@@ -6,26 +6,46 @@ import pandas as pd
 # Now load in the stack 
 RUN_NUMBER=14180
 
-directory_path = f"/media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/filtered/{RUN_NUMBER}/"
-outfile=f"Run_{RUN_NUMBER}_Filtered.h5"
- 
-file_paths = glob.glob(os.path.join(directory_path, '*.h5'))
-print(len(file_paths))
+mode="filt"
 
-# data = []
-data_properties = []
+if mode == "filt":
 
-for file_path in file_paths:
-    # data.append(pd.read_hdf(file_path, key = 'data'))
-    data_properties.append(pd.read_hdf(file_path, key = 'data_properties'))
+    directory_path = f"/media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/filtered/{RUN_NUMBER}/"
+    outfile=f"Run_{RUN_NUMBER}_Filtered.h5"
+    
+    file_paths = glob.glob(os.path.join(directory_path, '*.h5'))
+    print(len(file_paths))
 
-# data = pd.concat(data)
-data_properties  = pd.concat(data_properties)
+    data_properties = []
 
-# print(data)
-print(data_properties)
+    for file_path in file_paths:
 
-# Open the HDF5 file in write mode
-with pd.HDFStore(outfile, mode='w', complevel=5, complib='zlib') as store:
-    # store.put('data',data, format='table')
-    store.put('data_properties',data_properties, format='table')
+        data_properties.append(pd.read_hdf(file_path, key = 'data_properties'))
+
+    data_properties  = pd.concat(data_properties)
+
+    print(data_properties)
+
+    # Open the HDF5 file in write mode
+    with pd.HDFStore(outfile, mode='w', complevel=5, complib='zlib') as store:
+        store.put('data_properties',data_properties, format='table')
+
+else:
+    directory_path = f"/media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/filteredC/{RUN_NUMBER}/"
+    outfile=f"Run_{RUN_NUMBER}_FilteredC.h5"
+    
+    file_paths = glob.glob(os.path.join(directory_path, '*.h5'))
+    print(len(file_paths))
+
+    data = []
+
+    for file_path in file_paths:
+        data.append(pd.read_hdf(file_path, key = 'data'))
+
+    data = pd.concat(data)
+
+    print(data)
+
+    # Open the HDF5 file in write mode
+    with pd.HDFStore(outfile, mode='w', complevel=5, complib='zlib') as store:
+        store.put('data',data, format='table')
