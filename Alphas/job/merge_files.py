e287fbf4694f8183d468a94992c20e455177f735
Krishan Mistry <kvjmistry@gmail.com>
2024-12-04 17:57:23 -0600

    add histogram merger

--- Alphas/job/merge_files.py
+++ Alphas/job/merge_files.py
@@ -34,6 +34,7 @@ else:
     print("Combining Data Table")
     directory_path = f"/media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/filteredC/{RUN_NUMBER}/"
     outfile=f"Run_{RUN_NUMBER}_FilteredC.h5"
+    outfilehist=f"Run_{RUN_NUMBER}_Histograms.pkl"
     
     file_paths = glob.glob(os.path.join(directory_path, '*.h5'))
     print(len(file_paths))
@@ -52,3 +53,31 @@ else:
     # Open the HDF5 file in write mode
     with pd.HDFStore(outfile, mode='w', complevel=5, complib='zlib') as store:
         store.put('data',data, format='table')
+
+
+    directory_path = f"/media/argon/HardDrive_8TB/Krishan/NEXT100Data/alpha/filteredC/{RUN_NUMBER}/"
+    outfile=f"Run_{RUN_NUMBER}_HistogramMeta.h5"
+    
+    file_paths = glob.glob(os.path.join(directory_path, '*.pkl'))
+    print(len(file_paths))
+
+    histogram_df = []
+
+    total_hist = None
+
+    for file_path in file_paths:
+        histogram_df.append(pickle.load(pickle_file))
+        hist2D = pickle.load(pickle_file)
+
+        if total_hist is None:
+            total_hist = hist2D
+        else:
+            total_hist += hist2D
+
+    histogram_df = pd.concat(histogram_df)
+
+    print(histogram_df)
+
+    with open(outfilehist, 'wb') as pickle_file:
+    pickle.dump(histogram_df, pickle_file)
+    pickle.dump(total_hist, pickle_file)
