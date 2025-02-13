98eef1628b903bd1326ee3523348e09c181bb599
Krishan Mistry <kvjmistry@gmail.com>
2025-02-12 21:36:43 -0600

    Add photoelectric effect job scripts

---
+++ Alphas/job/RunPhotoelectric.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH -J PHOTOELECTRIC # A single job name for the array
+#SBATCH --nodes=1
+#SBATCH --mem 4000 # Memory request (6Gb)
+#SBATCH -t 0-1:00 # Maximum execution time (D-HH:MM)
+#SBATCH -o PHOTOELECTRIC_%A_%a.out # Standard output
+#SBATCH -e PHOTOELECTRIC_%A_%a.err # Standard error
+
+start=`date +%s`
+
+# Setup nexus and run
+echo "Setting up nexus"
+source /home/argon/Projects/Krishan/nexus/setup_cluster.sh
+source /home/argon/Projects/Krishan/venv/bin/activate
+
+JOBNAME="photoelectric"
+N_EVENTS=5000000
+CONFIG=NEXT100_S2_LT.config.mac
+INIT=NEXT100_S2_LT.config.mac
+
+# Create the directory
+cd /home/argon/Projects/Krishan/DoubleBeta/Alphas/
+mkdir -p $JOBNAME/jobid_"${SLURM_ARRAY_TASK_ID}"
+cd $JOBNAME/jobid_"${SLURM_ARRAY_TASK_ID}"
+
+# Calculate the unique seed number  
+SEED=$((${N_EVENTS}*(${SLURM_ARRAY_TASK_ID} - 1) + ${N_EVENTS}))
+echo "The seed number is: ${SEED}"
+
+# Change the config in the files
+sed -i "s#.*random_seed.*#/nexus/random_seed ${SEED}#" ${CONFIG}
+sed -i "s#.*start_id.*#/nexus/persistency/start_id ${SEED}#" ${CONFIG}
+sed -i "s#.*output_file.*#/nexus/persistency/output_file ${JOBNAME}_${SLURM_ARRAY_TASK_ID}#" ${CONFIG}
+
+# NEXUS
+echo "Running NEXUS" 
+nexus -n $N_EVENTS ${INIT}
+python3 /home/argon/Projects/Krishan/DoubleBeta/Alphas/compress_files.py ${JOBNAME}_${SLURM_ARRAY_TASK_ID}.h5
+
+echo "FINISHED....EXITING"
+
+end=`date +%s`
+let deltatime=end-start
+let hours=deltatime/3600
+let minutes=(deltatime/60)%60
+let seconds=deltatime%60
+printf "Time spent: %d:%02d:%02d\n" $hours $minutes $seconds 
---
+++ Alphas/scripts/compress_files.py
@@ -0,0 +1,35 @@
+# Python script to slim nexus files
+import sys
+import pandas as pd
+
+# python3 <basename>
+
+#config = pd.read_hdf(sys.argv[1], key = 'MC/configuration')
+#print(config)
+
+parts = pd.read_hdf(sys.argv[1], key = 'MC/particles')
+parts = parts[parts.primary == 1]
+parts = parts[(parts.initial_x > -500) & (parts.initial_x < 500) & (parts.initial_y > -500) & (parts.initial_y < 500)] # skim off events just outside the bin range
+print(parts)
+
+# Same with the hits
+#hits = pd.read_hdf(sys.argv[1], key = 'MC/hits')
+#print(hits)
+
+# Same with the sensor hits
+#sns_response = pd.read_hdf(sys.argv[1], key = 'MC/sns_response')
+#print(sns_response)
+
+# Same with the sensor positions
+#sns_positions = pd.read_hdf(sys.argv[1], key = 'MC/sns_positions')
+#print(sns_positions)
+
+
+# Open the HDF5 file in write mode
+with pd.HDFStore(f"{sys.argv[1]}", mode='w', complevel=5, complib='zlib') as store:
+    # Write each DataFrame to the file with a unique key
+    #store.put('MC/configuration', config, format='table')
+    store.put('MC/particles',parts, format='table')
+    #store.put('MC/hits',hits, format='table')
+    #store.put('MC/sns_response',sns_response, format='table')
+    #store.put('MC/sns_positions',sns_positions, format='table')
